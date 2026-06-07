27b444ad719ab3f63596d8e24fb8e561123ed05b
priyanshi tripathi <tripathipriyanshi681@gmail.com>
2026-06-07 16:39:20 +0530

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credencials=True
+
+)
+
 
 @app.get("/")
 def greeting():
